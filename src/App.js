8dc7b452284d01e431b04fb538785f209ab4cf39
annpermi <perminova.anna85@gmail.com>
2021-05-25 18:32:32 -0400

    Delete component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ import React, {useState} from 'react';
 import './App.css';
 
 
-function Todo({index, todo}){
+function Todo({todo, index, completeTodo, deleteTodo}){
   return (
-    <div className='todo'>{todo.text}</div>
-    
+    <div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}} className='todo'>{todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => deleteTodo(index)}>X</button>
+      </div>
+    </div>
+  )
+}
+
+function TodoForm({ addTodo }){
+  const [value, setValue] = useState('');
+  console.log(value)
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' className='input' value={value} onChange={e => setValue(e.target.value)} placeholder='Add'/>
+    </form>
   )
 }
 
@@ -19,11 +39,31 @@ function App() {
     isCompleted: false}
   ])
 
+  const addTodo = text => {
+    const newTodos = [...todos, {text}];
+    setTodos(newTodos)
+  }
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos)
+  }
+
+  const deleteTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos)
+  }
+
   return (
     <div className="app">
-      {todos.map((todo, index)=>{
-        return <Todo key={index} index={index} todo={todo}/>
-      })}
+      <div className='todo-list'>
+        <TodoForm addTodo={addTodo} />
+        {todos.map((todo, index)=>{
+          return <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} deleteTodo={deleteTodo}/>
+        })}
+      </div>
     </div>
   );
 }
